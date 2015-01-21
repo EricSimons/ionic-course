5831e48d8b425927806bd54cece6746d1df48e23
Eric Simons <ericsimons@es40.net>
2015-01-21 15:14:24 -0800

    service updates

--- code/songhop/www/js/services.js
+++ code/songhop/www/js/services.js
@@ -56,6 +56,11 @@ angular.module('songhop.services', [])
     // pop the index 0 off
     o.queue.shift();
 
+    // low on the queue? lets fill it up
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+
     return o.playCurrentSong();
   }
 
@@ -66,49 +71,18 @@ angular.module('songhop.services', [])
   return o;
 })
 
+
+
+
+
 /**
  * A simple example service that returns some data.
  */
 .factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+  
+  var o = {
 
-  // Some fake testing data
-  // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
-
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
   }
+
+  return o;
 });
