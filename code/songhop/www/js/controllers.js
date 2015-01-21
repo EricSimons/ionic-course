5ad2e1d7eab70b209d2710825fa28c5f0f8d1d3b
Eric Simons <ericsimons@es40.net>
2015-01-20 20:39:27 -0800

    app styling complete

--- code/songhop/www/js/controllers.js
+++ code/songhop/www/js/controllers.js
@@ -3,4 +3,5 @@ angular.module('songhop.controllers', [])
 .controller('DiscoverCtrl', function($scope) {})
 
 .controller('FavoritesCtrl', function($scope) {
+  $scope.items = [1,2,3,4,5,6,7,8,9,10,11,20];
 });
