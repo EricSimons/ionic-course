7cafc5b0e940dc4c3695c737211945e8b70cb525
Anand Raman <codestryke@outlook.com>
2015-07-04 01:20:40 -0700

    updated documentation with the correct app name

--- code/www/js/app.js
+++ code/www/js/app.js
@@ -1,7 +1,7 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'songhop' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('songhop', ['ionic', 'songhop.controllers'])
 
