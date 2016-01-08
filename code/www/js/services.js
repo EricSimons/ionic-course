d70a56f4c542b1ce78f9b7459896a0e977725716
mapineda Pineda <mapineda1222@gmail.com>
2016-01-08 11:32:55 -0600

    making improvements

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,6 +1,6 @@
-angular.module('songhop.services', [])
+angular.module('songhop.services', ['ionic.utils'])
 
-.factory('User', function($http, SERVER) {
+.factory('User', function($http, $q, $localstorage, SERVER) {
 	var o = {
 		username: false,
 		session_id: false, //TODO: sesson token should expire after x days
@@ -8,7 +8,7 @@ angular.module('songhop.services', [])
 		newFavorites: 0
 	}
 
-	  // attempt login or signup
+	// attempt login or signup
   	o.auth = function(username, signingUp) {
 
 	    var authRoute;
@@ -19,19 +19,71 @@ angular.module('songhop.services', [])
 	      authRoute = 'login';
 	    }
 
-    return $http.post(SERVER.url + '/' + authRoute, {username: username});
-  }
+	    return $http.post(SERVER.url + '/' + authRoute, {username: username})
+	    	.success(function(data) {
+	    		o.setSession(data.username, data.session_id, data.favorites);
+    	})
+  	}
+
+	// set session data
+	o.setSession = function(username, session_id, favorites) {
+		if (username) o.username = username;
+		if (session_id) o.session_id = session_id;
+		if (favorites) o.favorites = favorites;
+
+		// set data in localstorage object
+		$localstorage.setObject('user', { username: username, session_id: session_id });
+	}
+
+	// check if there's a user session present
+	o.checkSession = function() {
+		var defer = $q.defer();
+
+		if (o.session_id) {
+		  // if this session is already initialized in the service
+		  defer.resolve(true);
+
+		} else {
+		  // detect if there's a session in localstorage from previous use.
+		  // if it is, pull into our service
+		  var user = $localstorage.getObject('user');
+
+		  if (user.username) {
+		    // if there's a user, lets grab their favorites from the server
+		    o.setSession(user.username, user.session_id);
+		    o.populateFavorites().then(function() {
+		      defer.resolve(true);
+		    });
+
+		  } else {
+		    // no user info in localstorage, reject
+		    defer.resolve(false);
+		  }
+
+		}
+
+		return defer.promise;
+	}
+
+	// wipe out our session data
+	o.destroySession = function() {
+		$localstorage.setObject('user', {});
+		o.username = false;
+		o.session_id = false;
+		o.favorites = [];
+		o.newFavorites = 0;
+	}
 
 	o.addSongToFavorites = function(song) {
-   	 // make sure there's a song to add
+   	 	// make sure there's a song to add
     	if (!song) return false;
 
     	// add to favorites to front of array because you want the song to display at the top
     	o.favorites.unshift(song);
     	o.newFavorites++;
 
-    	// persist this to the server
-    return $http.post(SERVER.url + '/favorites', {session_id: o.session_id, song_id:song.song_id });
+	    // persist this to the server
+	    return $http.post(SERVER.url + '/favorites', {session_id: o.session_id, song_id:song.song_id });
   }
 
 	o.removeSongFromFavorites = function(song, index) {
