01ca7d8cfe25b1f1bd13cb0bf56bb3a5d6950c3d
mapineda Pineda <mapineda1222@gmail.com>
2016-01-08 05:54:10 -0600

    wrapping up core functionality

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,30 +4,35 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User) {
-	  // our first three songs
-  $scope.songs = [
-     {
-        "title":"Stealing Cinderella",
-        "artist":"Chuck Wicks",
-        "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
-        "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
-     },
-     {
-        "title":"Venom - Original Mix",
-        "artist":"Ziggy",
-        "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
-        "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
-     },
-     {
-        "title":"Do It",
-        "artist":"Rootkit",
-        "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
-        "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
-     }
-  ];
+.controller('DiscoverCtrl', function($scope, $timeout, $ionicLoading, User, Recommendations) {
+// helper functions for loading
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
 
-  $scope.currentSong = angular.copy($scope.songs[0]);
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  // set loading to true first time while we retrieve songs from server.
+  showLoading();
+// get our first songs
+ Recommendations.init()
+    .then(function(){
+
+      $scope.currentSong = Recommendations.queue[0];
+
+      return Recommendations.playCurrentSong();
+
+    })
+    .then(function(){
+      // turn loading off
+      hideLoading();
+      $scope.currentSong.loaded = true;
+    });
 
    // fired when we favorite / skip a song.
    $scope.sendFeedback = function (bool) {
@@ -38,15 +43,27 @@ Controller for the discover page
     $scope.currentSong.rated = bool;
     $scope.currentSong.hide = true;
 
+     // prepare the next song
+    Recommendations.nextSong();
+
     $timeout(function() {
-      // $timeout to allow animation to complete before changing to next song
-      // set the current song to one of our three songs
-      var randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
-
-      // update current song in scope
-      $scope.currentSong = angular.copy($scope.songs[randomSong]);
-
+      // $timeout to allow animation to complete
+      $scope.currentSong = Recommendations.queue[0];
+      $scope.currentSong.loaded = false;
     }, 250);
+
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    })
+
+  }
+
+  $scope.nextAlbumImage = function() {
+  	if (Recommendations.queue.length > 1) {
+  		return Recommendations.queue[1].image_large;
+  	}
+
+  	return '';
   }
 
 })
@@ -69,6 +86,16 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, Recommendations) {
+// stop audio when going to favorites page
+$scope.enteringFavorites = function() {
+    Recommendations.haltAudio();
+  }
+
+$scope.leavingFavorites = function() {
+    Recommendations.init();
+  } 
+
+
 
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -22,4 +22,70 @@ angular.module('songhop.services', [])
   }
 
 	return o;
+})
+
+.factory('Recommendations', function($http, $q, SERVER) {
+  var o = {
+    queue: []
+  };
+
+  var media;
+
+  o.init = function() {
+    if (o.queue.length === 0) {
+      // if there's nothing in the queue, fill it.
+      // this also means that this is the first call of init.
+      return o.getNextSongs();
+
+    } else {
+      // otherwise, play the current song
+      return o.playCurrentSong();
+    }
+  }
+
+  o.getNextSongs = function() {
+   	return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data){
+      // merge data into the queue
+      o.queue = o.queue.concat(data);
+    });
+  }
+
+   o.nextSong = function() {
+    // pop the index 0 off
+    o.queue.shift();
+
+    o.haltAudio();
+
+    // low on the queue? lets fill it up
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+
+  }
+
+   o.playCurrentSong = function() {
+   	var defer = $q.defer();
+
+    // play the current song's preview
+    media = new Audio(o.queue[0].preview_url);
+
+    // when song loaded, resolve the promise to let controller know.
+    media.addEventListener("loadeddata", function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  }
+
+  // used when switching to favorites tab
+  o.haltAudio = function() {
+    if (media) media.pause();
+  }
+
+  return o;
 });
